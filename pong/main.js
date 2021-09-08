fedb91d883b28cb745dd7dccc62f351d5f9bf06a
Darley36 <darleybest@gmail.com>
2021-09-08 09:40:06 -0500

    Queda listo la funcionalidad de pintar las barra y su accion de moverse

--- pong/main.js
+++ pong/main.js
@@ -13,7 +13,7 @@
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            elements.push(this.ball);
+            //elements.push(this.ball);
             return elements;
         }
     }
@@ -31,22 +31,29 @@
     }
 
     self.BoardView.prototype = {
+        clean: function(){
+            this.ctx.clearRect(0,0,this.board.width,this.board.height);
+        },
         draw:function(){
             for (var i = this.board.elements.length - 1; i >= 0;i--) {
                 var el = this.board.elements[i];
                 draw(this.ctx,el);
             }
+        },
+        play: function(){
+            this.clean();
+            this.draw();
         }
     }
 
     function draw(ctx,element){
-        if (element !== null && element.hasOwnProperty("kind")) {
-            switch(element.kind){
-                case "rectangle":
-                    ctx.fillRect(element.x,element.y,element.width,element.height);
-                    break;
-            }
+       
+        switch(element.kind){
+            case "rectangle":
+                ctx.fillRect(element.x,element.y,element.width,element.height);
+                break;
         }
+        
     }
 
 })();
@@ -79,21 +86,31 @@
 
 var board = new Board(800,400);
 var bar = new Bar(20,100,40,100,board);
-var bar = new Bar(735,100,40,100,board);
+var bar_2 = new Bar(735,100,40,100,board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas,board);
 
+
+
 document.addEventListener("keydown",function(ev){
+    ev.preventDefault();
     if (ev.keyCode == 38) {
         bar.up();
     }
     else if (ev.keyCode == 40) {
         bar.down();
+    }else if (ev.keyCode == 87) {
+        bar_2.up();
+    }else if (ev.keyCode == 83) {
+        bar_2.down();
     }
 
     console.log(bar.toString());
 })
-window.addEventListener("load",main);
-function main(){
+//window.addEventListener("load",main);
+window.requestAnimationFrame(controller);
+function controller(){
+    board_view.clean();
     board_view.draw();
+    window.requestAnimationFrame(controller);
 }
