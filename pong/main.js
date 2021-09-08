94d24e01993d4ba0bafb4ddd8ab0ed9fdacc6e35
Darley36 <darleybest@gmail.com>
2021-09-08 11:34:31 -0500

    Se agrega la funcion anonima de Ball estableciendo los parametros de la pelota y en la funcion draw se pinta la pelota

--- pong/main.js
+++ pong/main.js
@@ -13,9 +13,23 @@
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            //elements.push(this.ball);
+            elements.push(this.ball);
             return elements;
         }
+    }
+})();
+
+(function(){
+    self.Ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+
+        board.ball = this;
+        this.kind = "circle"
     }
 })();
 
@@ -52,11 +66,18 @@
             case "rectangle":
                 ctx.fillRect(element.x,element.y,element.width,element.height);
                 break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x,element.y,element.radius,0,7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
         
     }
 
 })();
+
 
 (function () {
     self.Bar = function(x,y,width,height,board){
@@ -89,6 +110,7 @@ var bar = new Bar(20,100,40,100,board);
 var bar_2 = new Bar(735,100,40,100,board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas,board);
+var ball = new Ball(350,100,10,board);
 
 
 
