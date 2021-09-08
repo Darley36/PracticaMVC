c1f943eb5c30b4be8fd1d9e8ff060c95810e90cf
Darley36 <darleybest@gmail.com>
2021-09-07 19:29:37 -0500

    Se crea el proyecto, a su vez tambien el archivo index.html y el main.js(con una funcion anonima)

---
+++ pong/main.js
@@ -0,0 +1,12 @@
+(function(){
+    self.Board = function(width,height){
+        this.width = width;
+        this.height = height;
+        this.playing = false;
+        this.game_over = false;
+    }
+})();
+
+function main(){
+
+}
